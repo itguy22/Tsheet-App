ea2211b8bf11c523620f6a3a8bcf96345b33ac08
Ryan Mendoza <mendozatech97@gmail.com>
2024-03-12 01:15:45 -0700

    Tsheet app beta version

--- tsheetproject/tsheetproject/urls.py
+++ tsheetproject/tsheetproject/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tsheetapp/', include('tsheetapp.urls')),
+    path('', include('tsheetapp.urls')),
 ]
